96a1af012a8aa4a6684f0ee74d768d155fddc802
Jacob Starry <jstarry@mail.bradley.edu>
2021-04-22 19:19:35 -0500

    Initial Commit

---
+++ CustomDrawerView/CustomDrawerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDrawerViewApp.swift
+//  CustomDrawerView
+//
+//  Created by Jacob Starry on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomDrawerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
